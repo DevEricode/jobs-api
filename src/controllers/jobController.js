2ba64d06a06e636f53e1114f5caacf5fca08f0c8
Eric Sampaio <ericstudy97@gmail.com>
2024-04-28 15:04:24 -0300

    :sparkles: feat: Created the method to update vacancy data.

--- src/controllers/jobController.js
+++ src/controllers/jobController.js
@@ -37,6 +37,18 @@ class JobController {
         }
     };
 
+    static async updateJob(req, res) {
+
+        const jobId = req.params.id;
+
+        try {
+            const updateJob = await job.findByIdAndUpdate(jobId, req.body);
+            res.status(200).json({ message: "Job vacancy updated successfully.",  job: updateJob});
+            
+        } catch (e) {
+            res.status(500).json({ message: `${e.message} - An error occurred in our system to update a vacancy.` });
+        }
+    };
 };
 
 export default JobController;
