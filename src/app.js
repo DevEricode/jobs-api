a31d2368473ef2c38c4ca855bd060312c3d7581a
Eric Sampaio <ericstudy97@gmail.com>
2024-04-28 13:28:42 -0300

    :sparkles: feat: The api connection was made with the database.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import express from "express";
 import routes from "./routes/index.js";
+import connectionDB from "./config/dbConnection.js";
 
 const app = express();
-
 routes(app);
+
+const connection = await connectionDB()
+
+
+connection.on("error", (e) => {
+    console.error("Houve um erro de conexão com o banco de dados!" + e);
+});
+
+connection.once("open",  () => { 
+    console.log("Conexão com o banco de dados efetuado com sucesso!");
+});
 
 export default app
