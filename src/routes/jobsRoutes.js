dacd057e59ed461d0256c2238db892b719e04581
Eric Sampaio <ericstudy97@gmail.com>
2024-05-04 08:32:49 -0300

    :sparkles: feat: Created the routes.

--- src/routes/jobsRoutes.js
+++ src/routes/jobsRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
+import JobController from "../controllers/jobController.js";
 
 const routes = express.Router();
 
-routes.get("/jobs", (req, res) => {
-    res.status(200).send("Testando");
-});
+routes.get("/jobs", JobController.getAllJobs);
+routes.get("/jobs/:id", JobController.getOneJob);
+routes.post("/jobs", JobController.createJob);
+routes.put("/jobs/:id", JobController.updateJob);
+routes.delete("/jobs/:id", JobController.deleteJob)
 
 
 export default routes; 
