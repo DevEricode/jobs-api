909eef1400bae8ea3375ad77d53fad851e990392
Eric Sampaio <ericstudy97@gmail.com>
2024-06-22 13:02:14 -0300

    :recycle: refactor: Refactoring the routes to contain the api version and adding the filterPagination middleware to the search route.

--- src/routes/jobsRoutes.js
+++ src/routes/jobsRoutes.js
@@ -1,15 +1,15 @@
 import express from "express";
 import JobController from "../controllers/jobController.js";
+import filtersPagination from "../middlewares/filtersPagination.js"
 
 const routes = express.Router();
 
-routes.get("/jobs", JobController.getAllJobs);
-routes.get("/jobs/workModel/search", JobController.searchByWorkModel);
-routes.get("/jobs/company/search", JobController.searchByCompany);
-routes.get("/jobs/:id", JobController.getOneJob);
-routes.post("/jobs", JobController.createJob);
-routes.put("/jobs/:id", JobController.updateJob);
-routes.delete("/jobs/:id", JobController.deleteJob)
+routes.get("/api/v1/jobs", JobController.getAllJobs);
+routes.get("/api/v1/jobs/search", JobController.searchByFilter, filtersPagination);
+routes.get("/api/v1/jobs/:id", JobController.getOneJob);
+routes.post("/api/v1/jobs", JobController.createJob);
+routes.put("/api/v1/jobs/:id", JobController.updateJob);
+routes.delete("/api/v1/jobs/:id", JobController.deleteJob)
 
 
 export default routes; 
